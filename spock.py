c34a599d7ff1a3561a96dca318718d946376ec7e
DeK <nofatclips@gmail.com>
2013-07-01 10:19:35 +0200

    Create spock.py

---
+++ spock.py
@@ -0,0 +1,25 @@
+from game import Game
+
+def declareP1Winner(p1Sign, p2Sign):
+    print ("Player One Wins: %s beats %s" % (p1Sign, p2Sign))
+
+def declareP2Winner(p1Sign, p2Sign):
+    print ("Player Two Wins: %s beats %s" % (p1Sign, p2Sign))
+    
+def callItADraw(p1Sign, p2Sign):
+    print ("It's draw between %s and %s" % (p1Sign, p2Sign))
+
+rpsls = Game ("rock", "paper", "scissors", "lizard", "spock") \
+    .setRules(rules) \
+    .setRules("""
+        scissors \
+            .beats(paper, lizard) \
+            .beats(spock, paper) \
+            .beats(rock, spock) \
+            .beats(scissors, rock) \
+            .beats(lizard, scissors)""") \
+            .whenPlayerOneWins(declareP1Winner) \
+            .whenPlayerTwoWins(declareP2Winner) \
+            .whenPlayersDraw(callItADraw) \
+
+rpsls.whoWins("lizard", "spock")
